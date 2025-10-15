fc650282d6e65c37a6f49534042a7b2b6ea6c254
小铭 <15332335062@qq.com>
2025-10-15 17:50:16 +0800

    初始化

--- index/pages/index/index.vue
+++ index/pages/index/index.vue
@@ -1,55 +1,42 @@
 <template>
 	<view class="content">
-		<view class="wrap">
-			<button type="primary" @click="openMP()">启动小程序</button>
-			<button type="primary" @click="openMP2Path()">启动水页面</button>
-		</view>
+		<button type="primary" v-for="(item,index) in navList" :key="index" @click="openMP()">
+			启动小程序
+		</button>
+		<button type="primary" @click="openMP2Path()">启动水页面</button>
 	</view>
 </template>
 
-<script>
+<script setup>
 	const mp = uni.requireNativePlugin('uniMP');
-	export default {
-		data() {
-			return {
-				appid: '__UNI__C814DC9',
-				wgt: 'https://liumingdepot.github.io/toolBox/__UNI__C814DC9'
-			}
-		},
-		methods: {
-			installMP(cb) {
-				// 获取应用
-				mp.getUniMPVersion(this.appid, (ret) => {
-					if (ret.code != 0) {
-						//获取失败时安装应用
-						mp.installUniMP({
-							appid: this.appid,
-							wgtFile: this.wgt
-						}, () => {
-							cb()
-						});
-					} else {
-						cb()
-					}
-				});
-			},
-			openMP() {
-				this.installMP(() => {
+
+	const bUrl = 'https://liumingdepot.github.io/toolBox'
+
+	function installMP(cb) {
+		// 获取应用
+		
+	}
+
+	function openMP(item) {
+		mp.getUniMPVersion(item.appid, (ret) => {
+			if (ret.code != 0) {
+				//获取失败时安装应用
+				mp.installUniMP({
+					appid: item.appid,
+					wgtFile: item.wgt
+				}, () => {
 					mp.openUniMP({
-						appid: this.appid,
+						appid: item.appid,
 						icon: '/static/logo.png'
 					})
+				});
+			} else {
+				mp.openUniMP({
+					appid: item.appid,
+					icon: '/static/logo.png'
 				})
-			},
-			openMP2Path() {
-				this.installMP(() => {
-					mp.openUniMP({
-						appid: this.appid,
-						path: 'pages/YA-ydjc/page1',
-					});
-				})
-			},
-		}
+			}
+		});
 	}
 </script>
 
@@ -59,31 +46,6 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #cf0000;
-	}
-
-	.wrap {
-		width: 690rpx;
-		margin-top: 30rpx;
-		padding: 0 30rpx;
 	}
 
 	button {
